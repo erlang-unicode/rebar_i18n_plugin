a454ee1bb0a3d8657f78e34a68494b68cb715a8a
Uvarov Michael <freeakk@gmail.com>
2012-01-06 21:49:21 +0400

    Add directory for non-existing headers.

--- src/rebar_i18n_plugin.erl
+++ src/rebar_i18n_plugin.erl
@@ -8,6 +8,7 @@
 %% standard rebar hooks
 -export([preprocess/2, compile/2]).
 
+-on_load(set_vars/0).
 
 
 %%
@@ -18,32 +19,50 @@ preprocess(Config, AppFile) ->
 	?DEBUG("Preprocess i18n\n", []),
 	ok.
 
+last(List, Count) ->
+    RevList = lists:reverse(List),
+    SubRevList = lists:sublist(RevList, Count),
+    lists:reverse(SubRevList).
+
 compile(Config, AppFile) ->
-	?DEBUG("Set env vars i18n\n", []),
-	export_env("CC", "icu-config --cc"),
-	export_env("CXX", "icu-config --cxx"),
-	export_env("ICU_CFLAGS", "icu-config --cflags"),
-	export_env("ICU_CXXFLAGS", "icu-config --cxxflags"),
-	export_env("ICU_LDFLAGS", "icu-config --ldflags"),
-	export_env("ICU_INC_PATH", "icu-config --cppflags-searchpath"),
-	case os:getenv("I18N_BUILD_ID") of
-	false ->
-		{Mega, Secs, _} = os:timestamp(),
-		Timestamp = Mega*1000000 + Secs,
-		os:putenv("I18N_BUILD_ID", [$.|integer_to_list(Timestamp)]);
-	_ -> ok
-	end,
-	ok.
+    set_vars(),
+    ok.
+
+
+set_vars() ->
+%   ?DEBUG("Set env vars i18n\n", []),
+    export_env("CC", "icu-config --cc"),
+    export_env("CXX", "icu-config --cxx"),
+    export_env("ICU_CFLAGS", "icu-config --cflags"),
+    export_env("ICU_CXXFLAGS", "icu-config --cxxflags"),
+    export_env("ICU_LDFLAGS", "icu-config --ldflags"),
+    export_env("ICU_INC_PATH", "icu-config --cppflags-searchpath",
+        fun(X) -> 
+            X ++ " -idirafter c_src/include " 
+        end),
+    case os:getenv("I18N_BUILD_ID") of
+    false ->
+        {Mega, Secs, _} = os:timestamp(),
+        Timestamp = Mega*1000000 + Secs,
+        os:putenv("I18N_BUILD_ID", [$.|integer_to_list(Timestamp)]);
+    _ -> ok
+    end,
+    ok.
 
 %%
 %% Internal Functions
 %%
 
 export_env(Name, Cmd) ->
+    FormatFn = fun(X) -> X end,
+    export_env(Name, Cmd, FormatFn).
+    
+
+export_env(Name, Cmd, FormatFn) ->
 	case os:getenv(Name) of
 	false ->
 		{0, Value} = eunit_lib:command(Cmd),
-		os:putenv(Name, Value),
+		os:putenv(Name, FormatFn(Value)),
 		ok;
 	_ -> ok
 	end.
